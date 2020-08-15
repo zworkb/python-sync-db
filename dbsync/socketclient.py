7180a3912d367fee57789ddea572647f73529937
Philipp Auersperg-Castell <phil@bluedynamics.com>
2020-08-15 10:02:58 +0200

    maxsize = 20M

--- dbsync/socketclient.py
+++ dbsync/socketclient.py
@@ -62,7 +62,7 @@ class GenericWSClient:
 
     async def connect_async(self, *, action: SocketAction = None, method: SocketMethod = None, path=""):
         print(f"before connecting to {self.uri(path)}")
-        ws = websockets.connect(self.uri(path), timeout=2000)
+        ws = websockets.connect(self.uri(path), timeout=2000, max_size=20 * 2 ** 20)
         res: Any = None
         try:
             async with ws as self.websocket:
