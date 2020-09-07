6969dec4c52eb114254da0db92147780aa62d72f
Philipp Auersperg-Castell <phil@bluedynamics.com>
2020-09-07 12:07:40 +0200

    rename because pypi has a dbsync

--- setup.py
+++ setup.py
@@ -1,12 +1,37 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+import os
+
+version = '0.8.0'
+shortdesc = "DB sync"
+
 
 setup(
     name='bddbsync',
-    version='0.8.0',
-    url='https://github.com/zworkb/python-sync-db',
-    author='Author Name',
+    version=version,
+    description=shortdesc,
+    # long_description=longdesc,
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='database sql sync',
+    author='BlueDynamics Alliance',
     author_email='phil@bluedynamics.com',
-    description='Database synchronisation',
+    url='http://github.com/zworkb/python-sync-db',
+    license='MIT',
     packages=find_packages(),
-    install_requires=['sqlalchemy', 'websockets', 'requests'],
+    # package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'sqlalchemy',
+        'websockets',
+        'requests',
+    ],
 )
